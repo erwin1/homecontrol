ff3ffe78fc7062cabdfa9d20ff3dd6d1e71a4649
Erwin Morrhey <erwin@lodgon.com>
2023-03-20 21:18:54 +0100

    bugfix charging state

--- src/main/java/evcharging/impl/tesla/TeslaEV.java
+++ src/main/java/evcharging/impl/tesla/TeslaEV.java
@@ -89,7 +89,7 @@ public class TeslaEV implements EV {
             int maxAmps = chargeState.getCharge_current_request_max();
             boolean currentlyCharging = chargeState.getCharging_state().equals("Charging")
                     || chargeState.getCharging_state().equals("Starting")
-                    || chargerState.equals(EVCharger.State.InProgress);
+                    || (chargerState != null && chargerState.equals(EVCharger.State.InProgress));
 
             LOGGER.log(Level.INFO, "Tesla charging state {0} ({1}A). Battery level {2}%", new Object[]{
                     chargeState.getCharging_state(),
